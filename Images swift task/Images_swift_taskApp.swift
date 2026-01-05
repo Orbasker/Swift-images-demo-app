5a0ff91920153bae2936f46cc945e6dbd453a0ce
or.basker <or.basker@ridewithvia.com>
2026-01-05 15:00:09 +0200

    Initial Commit

---
+++ Images swift task/Images_swift_taskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Images_swift_taskApp.swift
+//  Images swift task
+//
+//  Created by Or Basker on 05/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct Images_swift_taskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Images swift taskTests/Images_swift_taskTests.swift
@@ -0,0 +1,17 @@
+//
+//  Images_swift_taskTests.swift
+//  Images swift taskTests
+//
+//  Created by Or Basker on 05/01/2026.
+//
+
+import Testing
+@testable import Images_swift_task
+
+struct Images_swift_taskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
